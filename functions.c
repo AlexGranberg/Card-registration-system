c359957c72f08d521bdbdeea32db51d79fb4c3a0
AlexGranberg <alexandergranberg1983@gmail.com>
2023-03-29 10:25:43 +0200

    G klar

--- functions.c
+++ functions.c
@@ -19,7 +19,7 @@ while(1){
     int sel;
     if(GetInputInt("Select choice: ", &sel)) return sel;
     else{
-        printf("Not a valid entry!");
+        printf("Not a valid entry!\n");
     }
 }
 }
@@ -31,11 +31,16 @@ void remoteOpenDoor(){
 }
 
 void listCards(const State *state){
-    printf("Cards currently in the system: \n");
+    system("cls");
+    char key;
+    do{
+    printf("\nCards currently in the system: \n");
     for(int i = 0; i < state->antal; i++){
         
         printCards(&state->cards[i]);
     }
+    GetInput("\nPress any key to go back to meny.\n", &key, 1);
+    }while (key == '\n');
 }
 
 void createCard(State *state, const char *number) {
@@ -45,16 +50,18 @@ void createCard(State *state, const char *number) {
         state->cards = (Card *)malloc(1 * sizeof(Card));
     else
         state->cards = (Card *)realloc(state->cards, state->antal * sizeof(Card));
-    GetInput("Do you want to give access to this card? Y/N", answer, 10);
+    GetInput("Do you want to give access to this card? Y/N: ", answer, 10);
     bool access = (strcmp(answer, "Y") == 0 || strcmp(answer, "y") == 0);
     inputCard(&state->cards[state->antal-1], number, access);
+    state->cards[state->antal-1].access = access;
+    
 }
 
 
 void inputCard(Card *p, const char *number, bool access){
     strcpy(p->number, number);
     p->access = access;
-    
+
     time_t rawtime;
     struct tm *info;
 
@@ -63,8 +70,6 @@ void inputCard(Card *p, const char *number, bool access){
     info = localtime( &rawtime );
 
     strftime(p->buffer, sizeof(p->buffer),"%x-%I:%M%p", info);
-    
-
     
 }
     
@@ -99,42 +104,60 @@ void printCards(const Card *p){
 void cardInSystem(Card *p, State *state){
     char input[10];
     bool found = false;
+
+    bool access = false;
+   
     GetInput("Enter card number: ", input, 10);
 
     for(int i = 0; i < state->antal; i++){
         if(strcmp(input, state->cards[i].number) == 0 ){
-        found = true;
-        p = &state->cards[i];
-        break;
-        }
-        
+            found = true;
+            p = &state->cards[i];
+            break;
+        }   
     }
     if(found){
         char answer[10];
-        GetInput("Card is in system, do you want to add or remove access? Y/N", answer, 10);
+        GetInput("Card is in system, do you want to add or remove access? Y/N: ", answer, 10);
         if(strcmp(answer, "y") == 0 || strcmp(answer, "Y") == 0){
             if(p->access == true){
-            p->access = false;
-            printf("Access has been updated.\n");
+                p->access = false;
+                printf("Access has been updated.\n");
+                access = "No access";
             }
             else{
-            p->access = true;
+                p->access = true;
+                printf("Access has been updated.\n");
+                access = "Access";
             }
+           
         }
-        
-
+        else if(strcmp(answer, "n") == 0 || strcmp(answer, "N") == 0)
+            printf("Access has not been updated.\n");
+            
+        else{
+            printf("Not a valid entry!\n");
         }
+    }
     else{
         char answer[10];
-        GetInput("Card is not in system, do you want to add it? Y/N", answer, 10);
+        GetInput("Card is not in system, do you want to add it? Y/N: ", answer, 10);
         if(strcmp(answer, "y") == 0 || strcmp(answer, "Y") == 0){
             Card newCard;
             strcpy(newCard.number, input);
             inputCard(&newCard, input, p->access);
             createCard(state, input);
-            }
+            access = state->cards[state->antal-1].access;
+            printf("Card added to system with %saccess.\n", access ? "" : "No ");
+        }
+        else if(strcmp(answer, "n") == 0 || strcmp(answer, "N") == 0){
+            access = "No access";
+            printf("Card has not been added.\n");
+        }
+        else{
+            printf("Not a valid input!\n");
+        }
     }
-
 }
 
 
@@ -154,7 +177,6 @@ void fakeCardTest(Card *p, State *state){
     if(found){
         if(p->access == true){
             printf("\nCURRENTLY LAMP IS: GREEN\n\n");
-            sleep(3);
         }
         else{
             printf("\nCURRENTLY LAMP IS: RED\n\n");
